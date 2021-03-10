e4dca90303ece06824ca99b81c56c7ee13fd50fc
Ramona Maltan <ramonamaltan@gmail.com>
2021-03-10 10:04:51 +0100

    managing workout data with workout, running and cycling classes

--- app/script.js
+++ app/script.js
@@ -10,6 +10,47 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    this.pace = this.duration/ this.distance;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    this.speed = this.distance/ (this.duration / 60);
+  }
+}
+
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const bike1 = new Cycling([39, -12], 27, 95, 523);
+console.log(run1, bike1);
+
+///////////////////////////////////////////////////////////
+// APPLICATION ARCHITECTURE
 class App {
   #map;
   #mapEvent;
